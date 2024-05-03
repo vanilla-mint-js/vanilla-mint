ae1b3360c6cdbbe59ec78927f0916f17361c5734
icedlee337 <lee.e.norris@gmail.com>
2024-05-02 23:08:18 -0500

    chore: inconsequential change to qrcode

--- libs/components/src/lib/qr-code.class.ts
+++ libs/components/src/lib/qr-code.class.ts
@@ -18,20 +18,20 @@ export class QrCode extends VanillaMint<TAttrs> {
     }
 
     get qrcode() {
+        // todo: replace umd dep and delete this
+        // or... if this is how we roll, then we need to specify the umd name to avoid collisions
         return (window as any)[globalQrCodeName];
     }
 
     override async vmConnected() {
-
         if (!this.qrcode) {
             await injectScript(this, 'https://cdn.jsdelivr.net/npm/qrcode-generator/qrcode.min.js');
             (window as any)[globalQrCodeName] = (window as any).qrcode;
         }
 
         if (typeof this.qrcode !== 'function') {
-            console.warn(`${QrCode.name} component detected a global namespace error: "window.${globalQrCodeName}" has already been assigned`);
+            console.warn(`QrCode component detected a global namespace error: "window.${globalQrCodeName}" has already been assigned`);
         } else {
-            const qrcode = (window as any).qrcode;
             const typeNumber = 0;
             const errorCorrectionLevel = 'H';
 
@@ -41,7 +41,7 @@ export class QrCode extends VanillaMint<TAttrs> {
                     this.vmObserveAttr('text')]
                 ).pipe(
                     tap(([cellSize, text]) => {
-                        const qr = qrcode(typeNumber, errorCorrectionLevel);
+                        const qr = this.qrcode(typeNumber, errorCorrectionLevel);
                         qr.addData(text);
                         qr.make();
                         this.innerHTML = qr.createSvgTag({ cellSize, scalable: false });
