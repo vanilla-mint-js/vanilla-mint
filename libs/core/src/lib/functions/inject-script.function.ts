c9343a58b39a31a2bfd03dd8203f34a67e6688b6
icedlee337 <lee.e.norris@gmail.com>
2024-05-02 23:00:08 -0500

    fix: inject script

--- libs/core/src/lib/functions/inject-script.function.ts
+++ libs/core/src/lib/functions/inject-script.function.ts
@@ -2,14 +2,18 @@ import { appendChild } from "./append-child.function";
 
 export function injectScript(target: HTMLElement, src: string, type?: any) {
     return new Promise((resolve, reject) => {
+        const attrs: any = {
+            onload: resolve,
+            onerror: reject,
+            src,
+        };
+
+        if(type) {
+            attrs.type = type;
+        }
         appendChild(target, {
             tag: 'script',
-            attrs: {
-                onload: resolve,
-                onerror: reject,
-                src,
-                type,
-            }
+            attrs,
         });
     });
 }
