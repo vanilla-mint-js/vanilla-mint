ddc7badde626ee097aaeaf76f20550ea36958fb3
icedlee337 <lee.e.norris@gmail.com>
2025-03-05 15:13:17 -0600

    fix: compiler err

--- libs/forms/src/lib/types/form-layout.type.ts
+++ libs/forms/src/lib/types/form-layout.type.ts
@@ -1,7 +1,5 @@
-import { FormState, UseFormReturn } from "react-hook-form";
-
 type TFormFieldName<TFormData> = keyof TFormData;
-type TArbitraryRendererWithValue<TFormData> = (value: TFormData, formState: FormState<any>, form: UseFormReturn) => any;
+type TArbitraryRendererWithValue<TFormData> = (value: TFormData) => any;
 type TArbitraryRendererWithoutValue = () => any;
 type TArbitraryRenderer<TFormData> = TArbitraryRendererWithValue<TFormData> | TArbitraryRendererWithoutValue;
 export type TFormLayout<TFormData> = Array<Array<TArbitraryRenderer<TFormData> | TFormFieldName<TFormData>>>;
