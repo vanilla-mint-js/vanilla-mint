197a28ed637a8dfc8d1e3810b4f34683dd33b391
icedlee337 <lee.e.norris@gmail.com>
2025-03-05 15:29:06 -0600

    fix: another compiler err

--- libs/forms/src/index.ts
+++ libs/forms/src/index.ts
@@ -4,7 +4,6 @@ export * from './lib/constants/error-messages.constant';
 export * from './lib/constants/format-regexes.constant';
 
 export * from './lib/types/field-option.interface';
-export * from './lib/types/form-consumer-props.type';
 export * from './lib/types/form-control-props.type';
 export * from './lib/types/form-fields.type';
 export * from './lib/types/form-layout.type';
--- libs/forms/src/lib/types/form-consumer-props.type.ts
+++
@@ -1,3 +0,0 @@
-import { TOnSubmit } from "./on-submit.type";
-
-export type TFormConsumerProps<TFormData, TFormSchema = any> = { heading?: string, onSubmit: TOnSubmit<TFormData>, value?: TFormData, schema?: TFormSchema };
